0cccc0a91929e7a4ec6354582b57995a9a152d2d
Artyom Parfenenkov <tomredl.af@gmail.com>
2018-09-29 00:44:44 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
+
   // your implementation
+
+  let zeros = 0, reminder = number, divider = 5;
+
+  while (reminder >= 5) {
+
+    reminder = Math.floor(number / divider);
+
+    divider *= 5;
+
+    zeros += reminder;
+
+  }
+
+  return zeros;
+
 }
